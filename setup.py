d244e911cc0de1bd9f52856ddc0e14a2b88381d9
Benjamin J. Morgan <benmorgan2@gmail.com>
2014-04-13 21:48:38 +0100

    argument parsing bug in install.py fixed\nversion 0.0.1

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from distutils.core import setup
-    setup(
-        name = 'vasppy',
-        packages = ['vasppy'], # this must be the same as the name above
-        version = '0.0.1',
-        description = 'A library for manipulating VASP input / output files',
-        author = 'Benjamin J. Morgan',
-        author_email = 'bmorgan@liv.ac.uk',
-        url = 'https://github.com/bjmorgan/vasppy',   # use the URL to the github repo
-        download_url = 'https://github.com/bjmorgan/vasppy/tarball/0.1', # I'll explain this in a second
-        keywords = ['vasp'], # arbitrary keywords
-        classifiers = [],
-    )
+
+setup(
+    name = 'vasppy',
+    packages = ['vasppy'], # this must be the same as the name above
+    version = '0.0.1',
+    description = 'A library for manipulating VASP input / output files',
+    author = 'Benjamin J. Morgan',
+    author_email = 'bmorgan@liv.ac.uk',
+    url = 'https://github.com/bjmorgan/vasppy',   # use the URL to the github repo
+    download_url = 'https://github.com/bjmorgan/vasppy/tarball/0.0.1',
+    keywords = ['vasp'], # keywords
+    classifiers = []
+)

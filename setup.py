69d62c8ea6525b41c740e389c138e00b6871fdf9
Benjamin J. Morgan <benmorgan2@gmail.com>
2014-04-13 21:57:46 +0100

    version 0.0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name = 'vasppy',
     packages = ['vasppy'], # this must be the same as the name above
-    version = '0.0.1',
+    version = '0.0.2',
     description = 'A library for manipulating VASP input / output files',
     author = 'Benjamin J. Morgan',
     author_email = 'bmorgan@liv.ac.uk',

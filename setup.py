69e9177be8b5edb12eb49c9875911a34ea6dd2dc
Benjamin Morgan <benmorgan2@gmail.com>
2015-03-26 11:20:43 +0000

    email updated in setup.py

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
     name = 'vasppy',
     packages = ['vasppy'], # this must be the same as the name above
-    version = '0.0.2',
+    version = '0.0.3',
     description = 'A library for manipulating VASP input / output files',
     author = 'Benjamin J. Morgan',
-    author_email = 'bmorgan@liv.ac.uk',
+    author_email = 'bjm42@bath.ac.uk',
     url = 'https://github.com/bjmorgan/vasppy',   # use the URL to the github repo
-    download_url = 'https://github.com/bjmorgan/vasppy/tarball/0.0.1',
+    download_url = 'https://github.com/bjmorgan/vasppy/tarball/0.0.3',
     keywords = ['vasp'], # keywords
     classifiers = []
 )

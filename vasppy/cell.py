2d0d8a89f04b3b6ffc6764fa430a2fe42c855182
Benjamin Morgan <benmorgan2@gmail.com>
2015-06-18 17:44:19 +0100

    Added cell lattice rotation method to cell.py

--- vasppy/cell.py
+++ vasppy/cell.py
@@ -8,6 +8,22 @@ def angle( x, y ):
     cos_angle = dot / ( x_mod * y_mod )
     return np.degrees( np.arccos( cos_angle ) )
 
+def rotation_matrix(axis, theta):
+    """
+    Return the rotation matrix associated with counterclockwise rotation about
+    the given axis by theta radians.
+    """
+    axis = np.asarray (axis )
+    theta = np.asarray( theta )
+    axis = axis / math.sqrt( np.dot( axis, axis ) )
+    a = math.cos( theta / 2 )
+    b, c, d = -axis * math.sin( theta / 2 )
+    aa, bb, cc, dd = a*a, b*b, c*c, d*d
+    bc, ad, ac, ab, bd, cd = b*c, a*d, a*c, a*b, b*d, c*d
+    return np.array( [ [ aa+bb-cc-dd, 2*(bc+ad), 2*(bd-ac) ],
+                       [ 2*(bc-ad), aa+cc-bb-dd, 2*(cd+ab) ],
+                       [ 2*(bd+ac), 2*(cd-ab), aa+dd-bb-cc ] ] )
+
 class Cell:
 
     def __init__( self, matrix ):
@@ -67,3 +83,7 @@ class Cell:
 
     def unit_vectors( self ):
         return( ( self.matrix.transpose() / self.lengths() ).transpose() )
+
+    def rotate( self, axis, theta ):
+        self.matrix = np.array( [ np.dot( rotation_matrix(axis,theta), v) for v in self.matrix ] )
+

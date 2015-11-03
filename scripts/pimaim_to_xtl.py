b0eecf44486459fb5f6da64c7ee0d9092859b56d
Benjamin Morgan <benmorgan2@gmail.com>
2015-11-03 14:35:10 +0000

    Updated pimaim_to_xtl and moved general code to new vasppy/pimaim namespace

--- scripts/pimaim_to_xtl.py
+++ scripts/pimaim_to_xtl.py
@@ -4,6 +4,7 @@ import numpy as np
 import argparse
 from vasppy.poscar import Poscar
 from vasppy.cell import Cell
+import vasppy.pimaim
 
 def parse_command_line_arguments():
     parser = argparse.ArgumentParser( description = 'TODO' )
@@ -15,45 +16,8 @@ def parse_command_line_arguments():
         exit()
     return args 
 
-def lines_to_numpy_array( data ):
-    return np.array( [ [ float( s ) for s in line.split() ] for line in data ] )
-
-def read_pimaim_restart( filename, number_of_atoms ):
-    with open( filename, 'r' ) as f:
-        file_data = f.readlines()
-
-    cr_dump_log, vel_dump_log, chg_dump_log, full_dump_log = [ ( line.strip() == 'T' ) for line in file_data[:4] ]
-
-    # this assumes coordinates, velocities, and dipoles are all present.
-    # not sure what happens if atoms have qudrupoles, etc.
-    coordinates = lines_to_numpy_array( file_data[ 4 : 4 + number_of_atoms ] ) 
-    if vel_dump_log:
-        velocities = lines_to_numpy_array( file_data[ 4 + number_of_atoms : 4 + number_of_atoms * 2 ] ) 
-    else:
-        velocities = None
-    if chg_dump_log:
-        dipoles = lines_to_numpy_array( file_data[ 4 + number_of_atoms * 2 : 4 + number_of_atoms * 3 ] ) 
-    else:
-        dipoles = None
-    cell_matrix = lines_to_numpy_array( file_data[ -6: -3 ] )
-    cell_lengths = lines_to_numpy_array( file_data[ -3: ] )
-    full_cell_matrix = cell_matrix * cell_lengths
-    # TODO! need to check this with a non-orthorhombic cell
-    return( coordinates, velocities, dipoles, full_cell_matrix )
-
 if __name__ == '__main__':
     filename = 'testout.rst'
-    restart_file = True
-
     args = parse_command_line_arguments()
-    number_of_atoms = sum( args.atom_numbers )
-    coordinates, velocities, dipoles, full_cell_matrix = read_pimaim_restart( filename, number_of_atoms )
-
-    poscar = Poscar()
-    poscar.cell = Cell( full_cell_matrix ) # TODO: possibly this needs transposing?
-    poscar.atoms = args.labels
-    poscar.atom_numbers = args.atom_numbers
-    poscar.coordinate_type = 'Cartesian'
-    poscar.coordinates = coordinates
-
+    poscar = vasppy.pimaim.poscar_from_pimaim_restart( filename, args.atom_numbers, args.labels )
     poscar.output_as_xtl()
---
+++ vasppy/pimaim.py
@@ -0,0 +1,44 @@
+# routines for interacting with PIMAIM data / filetypes
+
+import numpy as np
+from vasppy.poscar import Poscar
+from vasppy.cell import Cell
+
+def lines_to_numpy_array( data ):
+    return np.array( [ [ float( s ) for s in line.split() ] for line in data ] )
+
+def read_restart_file( filename, number_of_atoms ):
+    with open( filename, 'r' ) as f:
+        file_data = f.readlines()
+
+    cr_dump_log, vel_dump_log, chg_dump_log, full_dump_log = [ ( line.strip() == 'T' ) for line in file_data[:4] ]
+
+    # this assumes coordinates, velocities, and dipoles are all present.
+    # not sure what happens if atoms have qudrupoles, etc.
+    coordinates = lines_to_numpy_array( file_data[ 4 : 4 + number_of_atoms ] )
+    if vel_dump_log:
+        velocities = lines_to_numpy_array( file_data[ 4 + number_of_atoms : 4 + number_of_atoms * 2 ] )
+    else:
+        velocities = None
+    if chg_dump_log:
+        dipoles = lines_to_numpy_array( file_data[ 4 + number_of_atoms * 2 : 4 + number_of_atoms * 3 ] )
+    else:
+        dipoles = None
+    cell_matrix = lines_to_numpy_array( file_data[ -6: -3 ] )
+    cell_lengths = lines_to_numpy_array( file_data[ -3: ] )
+    full_cell_matrix = cell_matrix * cell_lengths
+    # TODO! need to check this with a non-orthorhombic cell
+    return( coordinates, velocities, dipoles, full_cell_matrix )
+
+def poscar_from_pimaim_restart( filename, atom_numbers, atom_labels ):
+    number_of_atoms = sum( atom_numbers )
+    coordinates, velocities, dipoles, full_cell_matrix = read_restart_file( filename, number_of_atoms )
+
+    poscar = Poscar()
+    poscar.cell = Cell( full_cell_matrix ) # TODO: possibly this needs transposing
+    poscar.atoms = atom_labels
+    poscar.atom_numbers = atom_numbers
+    poscar.coordinate_type = 'Cartesian'
+    poscar.coordinates = coordinates
+ 
+    return poscar

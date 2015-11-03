845080f3a5c5fce40618cc6eafee55d88461e493
Benjamin Morgan <benmorgan2@gmail.com>
2015-11-03 14:24:00 +0000

    updated pimaim_to_xtl script

--- scripts/pimaim_to_xtl.py
+++ scripts/pimaim_to_xtl.py
@@ -18,23 +18,23 @@ def parse_command_line_arguments():
 def lines_to_numpy_array( data ):
     return np.array( [ [ float( s ) for s in line.split() ] for line in data ] )
 
-def read_pimaim_restart( filename ):
+def read_pimaim_restart( filename, number_of_atoms ):
     with open( filename, 'r' ) as f:
         file_data = f.readlines()
 
-    cr_dump_log, vel_dump_log, chg_dump_log, full_dump_log = [ bool( line.strip() ) for line in file_data[:4] ]
+    cr_dump_log, vel_dump_log, chg_dump_log, full_dump_log = [ ( line.strip() == 'T' ) for line in file_data[:4] ]
 
-    # figure out how many atoms are in this calculation
-    data_per_line = [ len( line.split() ) for line in file_data ] 
-
-    number_of_atomic_data_lines = next( index for index, d in enumerate( data_per_line[4:] ) if d == 1 )
-    number_of_atomic_data_types = sum( [ cr_dump_log, vel_dump_log, chg_dump_log ] )
-    number_of_atoms = int( number_of_atomic_data_lines / number_of_atomic_data_types )
     # this assumes coordinates, velocities, and dipoles are all present.
     # not sure what happens if atoms have qudrupoles, etc.
     coordinates = lines_to_numpy_array( file_data[ 4 : 4 + number_of_atoms ] ) 
-    velocities  = lines_to_numpy_array( file_data[ 4 + number_of_atoms : 4 + number_of_atoms * 2 ] ) 
-    dipoles     = lines_to_numpy_array( file_data[ 4 + number_of_atoms * 2 : 4 + number_of_atoms * 3 ] ) 
+    if vel_dump_log:
+        velocities = lines_to_numpy_array( file_data[ 4 + number_of_atoms : 4 + number_of_atoms * 2 ] ) 
+    else:
+        velocities = None
+    if chg_dump_log:
+        dipoles = lines_to_numpy_array( file_data[ 4 + number_of_atoms * 2 : 4 + number_of_atoms * 3 ] ) 
+    else:
+        dipoles = None
     cell_matrix = lines_to_numpy_array( file_data[ -6: -3 ] )
     cell_lengths = lines_to_numpy_array( file_data[ -3: ] )
     full_cell_matrix = cell_matrix * cell_lengths
@@ -46,7 +46,8 @@ if __name__ == '__main__':
     restart_file = True
 
     args = parse_command_line_arguments()
-    coordinates, velocities, dipoles, full_cell_matrix = read_pimaim_restart( filename )
+    number_of_atoms = sum( args.atom_numbers )
+    coordinates, velocities, dipoles, full_cell_matrix = read_pimaim_restart( filename, number_of_atoms )
 
     poscar = Poscar()
     poscar.cell = Cell( full_cell_matrix ) # TODO: possibly this needs transposing?

e756d3310af3b02635eecd1afe11d397b047686d
Benjamin Morgan <benmorgan2@gmail.com>
2016-07-03 21:22:24 +0100

    Modified k-point parser test to match floats instead of strings

--- tests/test_procar.py
+++ tests/test_procar.py
@@ -26,16 +26,16 @@ class ParserTestCase( unittest.TestCase ):
         """Checking that k-points are parsed from PROCAR format strings"""
         procar_string = " k-point    1 :    0.50000000 0.25000000 0.75000000     weight = 0.00806452\nk-point    2 :    0.50000000 0.25735294 0.74264706     weight = 0.00806452"
         self.assertEqual( procar.k_point_parser( procar_string ), 
-            [ ['0.50000000', '0.25000000', '0.75000000'], 
-              ['0.50000000', '0.25735294', '0.74264706'] ] )
+            [ [ 0.50000000, 0.25000000, 0.75000000 ], 
+              [ 0.50000000, 0.25735294, 0.74264706 ] ] )
 
     def test_negative_k_points_are_parsed( self ): 
         """Checking that negative k-points are parsed from PROCAR format strings"""
         procar_string = "  k-point  119 :   -0.01282051 0.00000000 0.00000000     weight = 0.00500000\n k-point  122 :    0.00000000-0.01282051 0.01282051     weight = 0.00500000\n k-point    1 :   -0.50000000 0.00000000-0.50000000     weight = 0.00500000"
         self.assertEqual( procar.k_point_parser( procar_string ), 
-            [ ['-0.01282051',  '0.00000000',  '0.00000000'], 
-              [ '0.00000000', '-0.01282051',  '0.01282051'],
-              ['-0.50000000',  '0.00000000', '-0.50000000']  ] )
+            [ [-0.01282051,  0.00000000,  0.00000000], 
+              [ 0.00000000, -0.01282051,  0.01282051],
+              [-0.50000000,  0.00000000, -0.50000000]  ] )
 
 if __name__ == '__main__':
     unittest.main()

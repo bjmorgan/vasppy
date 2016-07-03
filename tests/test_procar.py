575155d4b4c5d87a46c7d11b44424acef6be5f95
Benjamin Morgan <benmorgan2@gmail.com>
2016-07-03 21:13:45 +0100

    Added test for parsing negative k-points from PROCAR

--- tests/test_procar.py
+++ tests/test_procar.py
@@ -19,5 +19,23 @@ class ProcarTestCase( unittest.TestCase ):
         self.assertEqual( pcar.number_of_bands, 4 )
         self.assertEqual( pcar.number_of_k_points, 2 )
 
+class ParserTestCase( unittest.TestCase ):
+    """Test for VASP output parsers"""
+
+    def test_k_points_are_parsed( self ):
+        """Checking that k-points are parsed from PROCAR format strings"""
+        procar_string = " k-point    1 :    0.50000000 0.25000000 0.75000000     weight = 0.00806452\nk-point    2 :    0.50000000 0.25735294 0.74264706     weight = 0.00806452"
+        self.assertEqual( procar.k_point_parser( procar_string ), 
+            [ ['0.50000000', '0.25000000', '0.75000000'], 
+              ['0.50000000', '0.25735294', '0.74264706'] ] )
+
+    def test_negative_k_points_are_parsed( self ): 
+        """Checking that negative k-points are parsed from PROCAR format strings"""
+        procar_string = "  k-point  119 :   -0.01282051 0.00000000 0.00000000     weight = 0.00500000\n k-point  122 :    0.00000000-0.01282051 0.01282051     weight = 0.00500000\n k-point    1 :   -0.50000000 0.00000000-0.50000000     weight = 0.00500000"
+        self.assertEqual( procar.k_point_parser( procar_string ), 
+            [ ['-0.01282051',  '0.00000000',  '0.00000000'], 
+              [ '0.00000000', '-0.01282051',  '0.01282051'],
+              ['-0.50000000',  '0.00000000', '-0.50000000']  ] )
+
 if __name__ == '__main__':
     unittest.main()

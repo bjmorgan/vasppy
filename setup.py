f4c62f4a113290a9f3246384221bef2df18abb51
Benjamin Morgan <b.j.morgan@bath.ac.uk>
2017-04-21 14:35:11 +0100

    setup.py updates for 0.1.0 release

--- setup.py
+++ setup.py
@@ -3,16 +3,17 @@ from setuptools import setup
 setup(
     name = 'vasppy',
     packages = ['vasppy'], # this must be the same as the name above
-    version = '0.0.3',
+    version = '0.1.0',
     description = 'A library for manipulating VASP input / output files',
     author = 'Benjamin J. Morgan',
     author_email = 'bjm42@bath.ac.uk',
     url = 'https://github.com/bjmorgan/vasppy',   # use the URL to the github repo
-    download_url = 'https://github.com/bjmorgan/vasppy/tarball/0.0.3',
+    download_url = 'https://github.com/bjmorgan/vasppy/tarball/0.1.0',
     keywords = ['vasp'], # keywords
     install_requires = [
         'numpy',
-        'pymatgen'
+        'pymatgen',
+        'PyYAML'
     ],
     classifiers = []
 )

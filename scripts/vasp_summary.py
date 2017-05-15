a351f64392a996b08c5f0ba083a3daae9fc99c22
Benjamin Morgan <benmorgan2@gmail.com>
2017-05-15 13:34:54 +0100

    Started adding commandline options to vasp_summary script

--- scripts/vasp_summary.py
+++ scripts/vasp_summary.py
@@ -7,11 +7,23 @@ Expects a series of directories (listed in `result_dirs`) that each contain:
     vaspmeta.yaml (additional metadata providing information about each calculation)
 """
 
+import argparse
 from vasppy.summary import Summary, find_vasp_calculations
+
+def get_args():
+    parser = argparse.ArgumentParser( description='Summarise a VASP calculation.' )
+    parser.add_argument( '-r', '--recursive', help='Recursively analyse directories.', action='store_true' )
+    args = parser.parse_args()
+    return args
 
 to_print=[ 'title', 'status', 'stoichiometry', 'potcar', 'plus_u', 'energy', 'k-points', 'functional', 'encut', 'ediffg', 'ibrion', 'converged', 'md5', 'directory' ]
 
 if __name__ == "__main__":
-    for path in sorted( find_vasp_calculations() ):
-        s = Summary( path )
+    args = get_args()
+    if args.recursive:
+        path = sorted( find_vasp_calculations() )
+    else:
+        path = [ '.' ]
+    for p in path:
+        s = Summary( p )
         s.output( to_print=to_print )

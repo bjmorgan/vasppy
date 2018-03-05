bef918297630aac3cf66fa6cad5bb22ceb896cfe
Benjamin Morgan <benmorgan2@gmail.com>
2018-03-05 12:45:04 +0000

    Added more tests for Summary class

--- tests/test_summary.py
+++ tests/test_summary.py
@@ -6,6 +6,8 @@ from unittest.mock import Mock, patch, call
 from io import StringIO
 
 from vasppy.summary import Summary, md5sum, potcar_spec, find_vasp_calculations
+from vasppy.vaspmeta import VASPMeta
+
 from pymatgen.io.vasp.outputs import Vasprun
 
 mock_potcar_string = """foo
@@ -53,107 +55,109 @@ class SummaryTestCase( unittest.TestCase ):
     @patch('vasppy.summary.Summary.parse_vasprun')
     def setUp( self, mock_parse_vaspun, MockVASPMeta ):
         MockVASPMeta.from_file = Mock( return_value='foo' )
-        self.summary = Summary() 
+        self.summary = Summary()
+        self.summary.vasprun = Mock( spec=Vasprun )
+        self.summary.meta = Mock( spec=VASPMeta )
 
     def test_functional_not_PBE( self ):
-        summary = self.summary
-        summary.potcars_are_pbe = Mock( return_value=False )
-        self.assertEqual( summary.functional, 'not recognised' )
+        self.summary.potcars_are_pbe = Mock( return_value=False )
+        self.assertEqual( self.summary.functional, 'not recognised' )
        
     def test_functional_is_PBE( self ):
-        summary = self.summary
-        summary.potcars_are_pbe = Mock( return_value=True )
-        summary.vasprun = Mock( spec=Vasprun )
-        summary.vasprun.parameters = { 'GGA': 'PE' }
-        self.assertEqual( summary.functional, 'PBE' )
+        self.summary.potcars_are_pbe = Mock( return_value=True )
+        self.summary.vasprun.parameters = { 'GGA': 'PE' }
+        self.assertEqual( self.summary.functional, 'PBE' )
  
     def test_functional_is_PBEsol( self ):
-        summary = self.summary
-        summary.potcars_are_pbe = Mock( return_value=True )
-        summary.vasprun = Mock( spec=Vasprun )
-        summary.vasprun.parameters = { 'GGA': 'PS' }
-        self.assertEqual( summary.functional, 'PBEsol' )
+        self.summary.potcars_are_pbe = Mock( return_value=True )
+        self.summary.vasprun.parameters = { 'GGA': 'PS' }
+        self.assertEqual( self.summary.functional, 'PBEsol' )
  
     def test_functional_is_PW91( self ):
-        summary = self.summary
-        summary.potcars_are_pbe = Mock( return_value=True )
-        summary.vasprun = Mock( spec=Vasprun )
-        summary.vasprun.parameters = { 'GGA': '91' }
-        self.assertEqual( summary.functional, 'PW91' )
+        self.summary.potcars_are_pbe = Mock( return_value=True )
+        self.summary.vasprun.parameters = { 'GGA': '91' }
+        self.assertEqual( self.summary.functional, 'PW91' )
 
     def test_functional_is_rPBE( self ):
-        summary = self.summary
-        summary.potcars_are_pbe = Mock( return_value=True )
-        summary.vasprun = Mock( spec=Vasprun )
-        summary.vasprun.parameters = { 'GGA': 'RP' }
-        self.assertEqual( summary.functional, 'rPBE' )
+        self.summary.potcars_are_pbe = Mock( return_value=True )
+        self.summary.vasprun.parameters = { 'GGA': 'RP' }
+        self.assertEqual( self.summary.functional, 'rPBE' )
 
     def test_functional_is_AM05( self ):
-        summary = self.summary
-        summary.potcars_are_pbe = Mock( return_value=True )
-        summary.vasprun = Mock( spec=Vasprun )
-        summary.vasprun.parameters = { 'GGA': 'AM' }
-        self.assertEqual( summary.functional, 'AM05' )
+        self.summary.potcars_are_pbe = Mock( return_value=True )
+        self.summary.vasprun.parameters = { 'GGA': 'AM' }
+        self.assertEqual( self.summary.functional, 'AM05' )
 
     def test_functional_is_PBE0( self ):
-        summary = self.summary
-        summary.potcars_are_pbe = Mock( return_value=True )
-        summary.vasprun = Mock( spec=Vasprun )
-        summary.vasprun.parameters = { 'GGA': 'AM', 'LHFCALC': 'True', 'AEXX': '0.25' }
-        self.assertEqual( summary.functional, 'PBE0' )
+        self.summary.potcars_are_pbe = Mock( return_value=True )
+        self.summary.vasprun.parameters = { 'GGA': 'AM', 'LHFCALC': 'True', 'AEXX': '0.25' }
+        self.assertEqual( self.summary.functional, 'PBE0' )
    
     def test_functional_is_HSE06( self ):
-        summary = self.summary
-        summary.potcars_are_pbe = Mock( return_value=True )
-        summary.vasprun = Mock( spec=Vasprun )
-        summary.vasprun.parameters = { 'GGA': 'AM', 'LHFCALC': 'True', 'AEXX': '0.25', 'HFSCREEN': '0.2' }
-        self.assertEqual( summary.functional, 'HSE06' )
+        self.summary.potcars_are_pbe = Mock( return_value=True )
+        self.summary.vasprun.parameters = { 'GGA': 'AM', 'LHFCALC': 'True', 'AEXX': '0.25', 'HFSCREEN': '0.2' }
+        self.assertEqual( self.summary.functional, 'HSE06' )
   
     def test_functional_is_a_PBE_hybrid( self ):
-        summary = self.summary
-        summary.potcars_are_pbe = Mock( return_value=True )
-        summary.vasprun = Mock( spec=Vasprun )
-        summary.vasprun.parameters = { 'GGA': 'AM', 'LHFCALC': 'True', 'AEXX': '0.19' }
-        self.assertEqual( summary.functional, 'hybrid. alpha=0.19' )
+        self.summary.potcars_are_pbe = Mock( return_value=True )
+        self.summary.vasprun.parameters = { 'GGA': 'AM', 'LHFCALC': 'True', 'AEXX': '0.19' }
+        self.assertEqual( self.summary.functional, 'hybrid. alpha=0.19' )
 
     def test_functional_is_a_screened_PBE_hybrid( self ):
-        summary = self.summary
-        summary.potcars_are_pbe = Mock( return_value=True )
-        summary.vasprun = Mock( spec=Vasprun )
-        summary.vasprun.parameters = { 'GGA': 'AM', 'LHFCALC': 'True', 'AEXX': '0.19', 'HFSCREEN': '0.34' }
-        self.assertEqual( summary.functional, 'screened hybrid. alpha=0.19, mu=0.34' )
+        self.summary.potcars_are_pbe = Mock( return_value=True )
+        self.summary.vasprun.parameters = { 'GGA': 'AM', 'LHFCALC': 'True', 'AEXX': '0.19', 'HFSCREEN': '0.34' }
+        self.assertEqual( self.summary.functional, 'screened hybrid. alpha=0.19, mu=0.34' )
  
     def test_functional_raises_KeyError_if_PBE_tag_is_invalid( self ):
-        summary = self.summary
-        summary.potcars_are_pbe = Mock( return_value=True )
-        summary.vasprun = Mock( spec=Vasprun )
-        summary.vasprun.parameters = { 'GGA': 'foo' }
+        self.summary.potcars_are_pbe = Mock( return_value=True )
+        self.summary.vasprun.parameters = { 'GGA': 'foo' }
         with self.assertRaises( KeyError ):
-            summary.functional
+            self.summary.functional
 
     @patch('sys.stdout', new_callable=StringIO)
     def test_print_cbm( self, mock_stdout ):
         summary = self.summary
-        summary.vasprun = Mock( spec=Vasprun )
         summary.vasprun.eigenvalue_band_properties = [ 'null', 'CBM', 'VBM' ]
         summary.print_cbm()
         self.assertEqual( mock_stdout.getvalue(), 'cbm: CBM\n' )
 
     @patch('sys.stdout', new_callable=StringIO)
     def test_print_vbm( self, mock_stdout ):
-        summary = self.summary
-        summary.vasprun = Mock( spec=Vasprun )
-        summary.vasprun.eigenvalue_band_properties = [ 'null', 'CBM', 'VBM' ]
-        summary.print_vbm()
+        self.summary.vasprun.eigenvalue_band_properties = [ 'null', 'CBM', 'VBM' ]
+        self.summary.print_vbm()
         self.assertEqual( mock_stdout.getvalue(), 'vbm: VBM\n' )
 
     @patch('sys.stdout', new_callable=StringIO)
     def test_print_converged( self, mock_stdout ):
-        summary = self.summary
-        summary.vasprun = Mock( spec=Vasprun )
-        summary.vasprun.converged = 'conv'
-        summary.print_converged()
+        self.summary.vasprun.converged = 'conv'
+        self.summary.print_converged()
         self.assertEqual( mock_stdout.getvalue(), 'converged: conv\n' )
+
+    def test_potcars_are_pbe_if_true( self ):
+        self.summary.vasprun.potcar_symbols = [ 'PAW_PBE Fe_pv 06Sep2000', 'PAW_PBE O 08Apr2002' ]
+        self.assertTrue( self.summary.potcars_are_pbe() )
+
+    def test_potcars_are_pbe_if_false( self ):
+        self.summary.vasprun.potcar_symbols = [ 'foo', 'PAW_PBE O 08Apr2002' ]
+        self.assertFalse( self.summary.potcars_are_pbe() )
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_print_type( self, mock_stdout ):
+        self.summary.meta.type = 'TYPE'
+        self.summary.print_type()
+        self.assertEqual( mock_stdout.getvalue(), 'type: TYPE\n' )
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_print_type_if_type_is_not_set( self, mock_stdout ):
+        self.summary.meta.type = None
+        self.summary.print_type()
+        self.assertEqual( mock_stdout.getvalue(), '' )
+
+    @patch('sys.stdout', new_callable=StringIO)
+    def test_print_title( self, mock_stdout ):
+        self.summary.meta.title = 'TITLE'
+        self.summary.print_title()
+        self.assertEqual( mock_stdout.getvalue(), 'title: TITLE\n' )
 
 class SummaryHelperFunctionsTestCase( unittest.TestCase ):
 

c2dad2f4285fecff54a6cb62664244db433b282c
Benjamin Morgan <b.j.morgan@bath.ac.uk>
2018-10-30 16:24:18 +0000

    Added LDA and GGA (PW91) pseudopotential hashes

--- vasppy/data/potcar_data.py
+++ vasppy/data/potcar_data.py
@@ -6,7 +6,7 @@ my_path = os.path.dirname(__file__)
 potcar_md5sum_data = {}
 potcar_nelect = {}
 
-potcar_sets = [ 'PBE', 'PBE_52', 'PBE_54' ]
+potcar_sets = [ 'PBE', 'PBE_52', 'PBE_54', 'GGA', 'USPP_GGA', 'LDA', 'LDA_52', 'LDA_54', 'USPP_LDA' ]
 
 for potcar_set in potcar_sets:
     with open( '{}_md5.yaml'.format( os.path.join( my_path, potcar_set ) ), 'r' ) as stream:

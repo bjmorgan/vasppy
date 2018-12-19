3435afda970637d42265418e457c9572fa45e21f
connorourke <conn.orourke@gmail.com>
2018-12-19 14:28:29 +0000

    Update to pimaim.py for non-orthogonal cells

--- vasppy/pimaim.py
+++ vasppy/pimaim.py
@@ -27,14 +27,20 @@ def read_restart_file( filename, number_of_atoms ):
     cell_lengths = lines_to_numpy_array( file_data[ -3: ] ) * 0.52918 # convert bohr to Angstroms
     full_cell_matrix = cell_matrix * cell_lengths
     # TODO! need to check this with a non-orthorhombic cell
-    return( coordinates, velocities, dipoles, full_cell_matrix )
+    return( coordinates, velocities, dipoles, full_cell_matrix, cell_lengths )
+
+def get_cart_coords_from_pimaim_restart(coordinates, full_cell_matrix, cell_lengths):
+
+    temp = np.array([full_cell_matrix[i]/cell_lengths[i] for i in range(3)])
+
+    return(np.dot(coordinates,temp.transpose()))
 
 def poscar_from_pimaim_restart( filename, atom_numbers, atom_labels ):
-    print("Gonna convert from pimaim to poscar")
     number_of_atoms = sum( atom_numbers )
-    coordinates, velocities, dipoles, full_cell_matrix = read_restart_file( filename, number_of_atoms )
+    coordinates, velocities, dipoles, full_cell_matrix, cell_lengths = read_restart_file( filename, number_of_atoms, cell_lengths )
 
     poscar = Poscar()
+    coordinates = get_cart_coords_from_pimaim_restart(coordinates, full_cell_matrix,cell_lengths)
     poscar.cell = Cell( full_cell_matrix .transpose()) # TODO: possibly this needs transposing
     poscar.atoms = atom_labels
     poscar.atom_numbers = atom_numbers

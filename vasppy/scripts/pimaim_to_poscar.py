3150e7a75ebd7b7ba54cde643ddc211a60615326
Benjamin Morgan <benmorgan2@gmail.com>
2018-12-20 09:11:09 +0000

    Tidying pimaim_to_poscar script

--- vasppy/scripts/pimaim_to_poscar.py
+++ vasppy/scripts/pimaim_to_poscar.py
@@ -22,12 +22,9 @@ def lines_to_numpy_array( data ):
 def read_pimaim_restart( filename ):
     with open( filename, 'r' ) as f:
         file_data = f.readlines()
-
     cr_dump_log, vel_dump_log, chg_dump_log, full_dump_log = [ bool( line.strip() ) for line in file_data[:4] ]
-
     # figure out how many atoms are in this calculation
     data_per_line = [ len( line.split() ) for line in file_data ] 
-
     number_of_atomic_data_lines = next( index for index, d in enumerate( data_per_line[4:] ) if d == 1 )
     number_of_atomic_data_types = sum( [ cr_dump_log, vel_dump_log, chg_dump_log ] )
     number_of_atoms = int( number_of_atomic_data_lines / number_of_atomic_data_types )
@@ -39,17 +36,11 @@ def read_pimaim_restart( filename ):
     cell_matrix = lines_to_numpy_array( file_data[ -6: -3 ] )
     cell_lengths = lines_to_numpy_array( file_data[ -3: ] )
     full_cell_matrix = cell_matrix * cell_lengths
-    
     return( coordinates, velocities, dipoles, full_cell_matrix, cell_lengths)
-
-#def get_cart_coords_from_pimaim_restart(coordinates, full_cell_matrix, cell_lengths):
-
-#   return(np.dot(coordinates,np.array([full_cell_matrix[i]/cell_lengths[i] for i in range(3)])))
 
 def main():
     filename = 'testout.rst'
     restart_file = True
-
     args = parse_command_line_arguments()
     coordinates, velocities, dipoles, full_cell_matrix, cell_lengths = read_pimaim_restart( filename )
     assert( sum( args.atom_numbers ) == len( coordinates ) )
@@ -59,7 +50,6 @@ def main():
     poscar.cell = Cell( full_cell_matrix)
     poscar.atoms = args.labels
     poscar.atom_numbers = args.atom_numbers
-
     poscar.coordinate_type = 'Cartesian'
     poscar.coordinates = coordinates
     poscar.output()

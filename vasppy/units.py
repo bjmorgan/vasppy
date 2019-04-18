ca0f603c6ed2419ed9c0c5b222f234bc8bfcd299
Benjamin Morgan <benmorgan2@gmail.com>
2019-04-18 22:01:39 +0100

    eV to Hartree conversion now uses scipy.constants

--- vasppy/units.py
+++ vasppy/units.py
@@ -1,3 +1,4 @@
 from scipy.constants import physical_constants, angstrom
 
 angstrom_to_bohr = physical_constants['atomic unit of length'][0] / angstrom
+ev_to_hartree = physical_constants['electron volt-hartree relationship'][0]

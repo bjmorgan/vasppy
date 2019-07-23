cca75bd559cc38b54d1e0430572de85bf095aef6
Benjamin Morgan <benmorgan2@gmail.com>
2019-07-23 14:37:53 +0100

    Added filename test for OUTCAR parser

--- tests/test_outcar.py
+++ tests/test_outcar.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import Mock, patch, mock_open
+from unittest.mock import Mock, patch, mock_open, call
 
 from vasppy.outcar import final_energy_from_outcar, potcar_eatom_list_from_outcar
 
@@ -13,6 +13,14 @@ class OutcarTestCase( unittest.TestCase ):
                        energy without entropy =    -2997.63289805  energy(sigma->0) =    -2997.63289805\n"""    
         with patch( 'builtins.open', mock_open( read_data=example_file ), create=True ) as m:
             self.assertEqual( final_energy_from_outcar(), -2997.63289805 )
+
+    def test_final_energy_from_outcar_with_filename(self):
+        example_file = """energy without entropy =    -2997.63294724  energy(sigma->0) =    -2997.63294724\n
+                       energy  without entropy=    -2997.63294724  energy(sigma->0) =    -2997.63294724\n
+                       energy without entropy =    -2997.63289805  energy(sigma->0) =    -2997.63289805\n"""    
+        with patch('builtins.open', mock_open(read_data=example_file), create=True) as m:
+            final_energy_from_outcar(filename='foo')
+            self.assertEqual( m.mock_calls[0], call('foo') )
 
     def test_potcar_eatom_list_from_potcar( self ):
         example_file = """energy of atom  1       EATOM=-1042.3781\n

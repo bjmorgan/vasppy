35703e2399680f09a3275a1c8d2a1431413630b0
Benjamin Morgan <benmorgan2@gmail.com>
2019-07-29 21:47:54 +0100

    Added simplest test for Poscar.output()

--- tests/test_poscar.py
+++ tests/test_poscar.py
@@ -4,6 +4,8 @@ from unittest.mock import Mock, patch, mock_open
 import vasppy.poscar 
 import numpy as np
 from collections import Counter
+import io
+import sys
 
 class PoscarTestCase( unittest.TestCase ):
 
@@ -12,6 +14,17 @@ class PoscarTestCase( unittest.TestCase ):
         poscar.atoms = [ 'A', 'B', 'C' ]
         poscar.atom_numbers = [ 1, 2, 3 ]
         self.assertEqual( poscar.stoichiometry, Counter( { 'A': 1, 'B': 2, 'C': 3 } ) )
+
+    def test_output( self ):
+        poscar = vasppy.poscar.Poscar()
+        poscar.atoms = [ 'A', 'B', 'C' ]
+        poscar.atom_numbers = [ 1, 2, 3 ]
+        captured_output = io.StringIO()
+        sys.stdout = captured_output
+        poscar.output_coordinates_only = Mock()
+        poscar.output()
+        sys.stdout = sys.__stdout__
+        print( captured_output.getvalue() )
 
 if __name__ == '__main__':
     unittest.main()

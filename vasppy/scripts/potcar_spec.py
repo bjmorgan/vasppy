469bb738e9b980b2060fe71d3d0e66748fba809d
Benjamin Morgan <benmorgan2@gmail.com>
2020-06-15 18:16:19 +0100

    Added potcar_spec option to return md5 hashes

--- vasppy/scripts/potcar_spec.py
+++ vasppy/scripts/potcar_spec.py
@@ -5,14 +5,22 @@ import argparse
 
 def parse_command_line_arguments():
     parser = argparse.ArgumentParser( description='Generate POTCAR specification based on hashing individual pseudopotential strings' )
-    parser.add_argument( 'potcar', help="filename of the VASP POTCAR to be processed", nargs='?', default='POTCAR' )
+    parser.add_argument('potcar', help="filename of the VASP POTCAR to be processed", nargs='?', default='POTCAR' )
+    parser.add_argument('--hash', help="return the md5 hashes of the individual pseudopotential strings", action='store_true') 
     args = parser.parse_args()
     return args
 
 def main():
     args = parse_command_line_arguments()
-    for p, ps in potcar_spec( args.potcar ).items():
-        print( p, ps )
+    if args.hash:
+        hashes = {}
+        for p, md5hash in potcar_spec(args.potcar, return_hashes=True).items():
+            hashes[p] = md5hash
+    for p, ps in potcar_spec(args.potcar).items():
+        if args.hash:
+            print(p, ps, hashes[p])
+        else:
+            print(p, ps, hashes[p])
 
 if __name__ == '__main__':
     main()

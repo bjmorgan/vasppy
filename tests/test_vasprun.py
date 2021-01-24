563d594eee8e102b146cf91b554a5f7bd4077706
Benjamin Morgan <benmorgan2@gmail.com>
2021-01-24 09:05:05 +0000

    Implemented generic vasprun <varray> parser.
    Closes https://github.com/bjmorgan/vasppy/issues/16

--- tests/test_vasprun.py
+++ tests/test_vasprun.py
@@ -1,15 +1,18 @@
 import unittest
-from vasppy.vasprun import Vasprun
+from vasppy.vasprun import Vasprun, parse_varray
 from unittest.mock import Mock, patch, call, mock_open
 from lxml import etree
 from io import BytesIO
+
+def vasprun_from_xml_string(xml_string):
+    dummy_xml_file = BytesIO(xml_string.encode('ascii'))
+    return Vasprun(dummy_xml_file)
 
 class TestVasprun(unittest.TestCase):
 
     def test_vasprun_is_initialised(self):
         dummy_xml = "<root>data</root>" 
-        dummy_xml_file = BytesIO(dummy_xml.encode('ascii'))
-        vasprun = Vasprun(dummy_xml_file)
+        vasprun = vasprun_from_xml_string(dummy_xml)
         self.assertEqual(etree.tostring(vasprun.doc).decode('ascii'), dummy_xml)
         for element in vasprun.doc.iter():
             self.assertEqual(element.tag, 'root')
@@ -28,16 +31,14 @@ class TestVasprun(unittest.TestCase):
                      "    </array>\n"
                      "  </atominfo>\n"
                      "</root>")
-        dummy_xml_file = BytesIO(dummy_xml.encode('ascii'))
-        vasprun = Vasprun(dummy_xml_file)
+        vasprun = vasprun_from_xml_string(dummy_xml)
         self.assertEqual(vasprun.atom_names(), ['Nb', 'O', 'F', 'F'])
 
     def test_atom_names_raises_valueerror_if_no_atominfo_found(self):
         dummy_xml = ("<root>\n"
                      "  <foo>bar</foo>\n"
                      "</root>")
-        dummy_xml_file = BytesIO(dummy_xml.encode('ascii'))
-        vasprun = Vasprun(dummy_xml_file)
+        vasprun = vasprun_from_xml_string(dummy_xml)
         with self.assertRaises(ValueError):
             vasprun.atom_names()
    
@@ -46,10 +47,39 @@ class TestVasprun(unittest.TestCase):
                      "  <atominfo>\n"
                      "  </atominfo>\n"
                      "</root>")
-        dummy_xml_file = BytesIO(dummy_xml.encode('ascii'))
-        vasprun = Vasprun(dummy_xml_file)
+        vasprun = vasprun_from_xml_string(dummy_xml)
         with self.assertRaises(ValueError):
             vasprun.atom_names()
-   
+  
+    def test_parse_varray_parses_type_equals_float(self):
+        elem = etree.Element("varray")
+        v1 = etree.SubElement(elem, "v")
+        v2 = etree.SubElement(elem, "v")
+        v1.text = "0.1   0.2   0.3"
+        v2.text = "0.4   0.5   0.6"
+        expected_data = [[0.1, 0.2, 0.3], [0.4, 0.5, 0.6]]
+        self.assertEqual(parse_varray(elem), expected_data)
+
+    def test_parse_varray_parses_type_equals_float(self):
+        elem = etree.Element("varray")
+        elem.set("type", "int")
+        v1 = etree.SubElement(elem, "v")
+        v2 = etree.SubElement(elem, "v")
+        v1.text = "1   2   3"
+        v2.text = "4   5   6"
+        expected_data = [[1, 2, 3], [4, 5, 6]]
+        self.assertEqual(parse_varray(elem), expected_data)
+
+    def test_parse_varray_parses_type_equals_logical(self):
+        elem = etree.Element("varray")
+        elem.set("type", "logical")
+        v1 = etree.SubElement(elem, "v")
+        v2 = etree.SubElement(elem, "v")
+        v1.text = "T   T   F"
+        v2.text = "F   T   F"
+        expected_data = [[True, True, False], [False, True, False]]
+        self.assertEqual(parse_varray(elem), expected_data)
+
+
 if __name__ == '__main__':
     unittest.main()
--- vasppy/vasprun.py
+++ vasppy/vasprun.py
@@ -1,5 +1,29 @@
 from lxml import etree # type: ignore
-from typing import List
+from typing import List, Union, TypeVar
+TNum = TypeVar('TNum', int, float)
+
+def parse_varray(varray: etree.Element) -> Union[List[List[float]], 
+                                                 List[List[int]],
+                                                 List[List[bool]]]:
+    """Parse <varray> data.
+
+    Args:
+        varray (etree.Element): xml <varray> element.
+
+    Returns:
+        (list(list): A nested list of either float, int, or bool.
+
+    """
+    m: Union[List[List[int]], List[List[float]], List[List[bool]]]
+    varray_type = varray.get("type", None)
+    v_list = [v.text.split() for v in varray.findall("v")] 
+    if varray_type == 'int':
+        m = [[int(number) for number in v] for v in v_list]
+    elif varray_type == 'logical':
+        m = [[i == "T" for i in v] for v in v_list]
+    else:
+        m = [[float(number) for number in v] for v in v_list]
+    return m
 
 class Vasprun:
     """Object for parsing vasprun.xml data."""

3e52377c261c357e45efacac9739797cf96eea97
Patrick J. Taylor <pjt35@bath.ac.uk>
2021-10-28 18:24:42 +0100

    Implement pymatgen functional detection

--- vasppy/summary.py
+++ vasppy/summary.py
@@ -196,63 +196,12 @@ class Summary:
     def functional( self ):
         """
         String description of the calculation functional.
-       
-        Recognises:
-            - PBE
-            - PBEsol
-            - PBE-based hybrids:
-                - PBE0 (alpha=0.25, no screening)
-                - HSE06 (alpha=0.25, mu=0.2)
-                - generic hybrids (alpha=?, no screening)
-                - generic screened hybrids (alpha=?, mu=?)
         
         Returns:
             (Str): String describing the calculation functional.
 
         """
-        if self.potcars_are_pbe(): # PBE base funtional
-            if 'LHFCALC' in self.vasprun.parameters:
-                alpha = float( self.vasprun.parameters['AEXX'] )
-            else:
-                alpha = 0.0
-            if 'HFSCREEN' in self.vasprun.parameters:
-                mu = float( self.vasprun.parameters['HFSCREEN'] )
-            else:
-                mu = 0
-            if alpha > 0:
-                if mu > 0: # screened hybrid
-                    if ( mu == 0.2 ) and ( alpha == 0.25 ):
-                        f = 'HSE06'
-                    else:
-                        f = "screened hybrid. alpha={}, mu={}".format( alpha, mu )
-                else: # unscreened hybrid
-                    if alpha == 0.25:
-                        f = 'PBE0'
-                    else: 
-                        f = "hybrid. alpha={}".format( alpha )
-            else: # not hybrid. Plain PBE or some variant.
-                pbe_list = { 'PS': 'PBEsol',
-                             'PE': 'PBE',
-                             '91': 'PW91',
-                             'RP': 'rPBE',
-                             'AM': 'AM05' }
-                try:
-                    f = pbe_list[ self.vasprun.parameters['GGA'].upper() ]
-                except: # meta-GGA
-                    meta_gga_list = {'SCAN' : 'SCAN', 
-                                     'RSCAN' : 'rSCAN', 
-                                     'R2SCAN' : 'r2SCAN', 
-                                     'TPSS' : 'TPSS', 
-                                     'RTPSS' : 'RTPSS', 
-                                     'M06L' : 'M06-L', 
-                                     'MBJ' : 'MBJ', 
-                                     'MS0' : 'MS0', 
-                                     'MS1' : 'MS1', 
-                                     'MS2' : 'MS2'}
-                    f = meta_gga_list[ self.vasprun.incar['METAGGA'].upper() ]
-        else:
-            f = 'not recognised'    
-        return f
+        return self.vasprun.run_type
 
     def potcars_are_pbe( self ):
         return all( 'PBE' in s for s in self.vasprun.potcar_symbols )

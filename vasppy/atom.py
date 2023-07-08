6c63b22b188a7c5de845af2312b6860e5f6fd6f1
Benjamin <b.j.morgan@bath.ac.uk>
2023-07-08 10:08:14 +0100

    more typing

--- vasppy/atom.py
+++ vasppy/atom.py
@@ -1,9 +1,13 @@
+import numpy as np
+
 class Atom:
     """
     Class for individual atoms
     """
 
-    def __init__( self, label, r ): # currently assume fractional coordinates
+    def __init__(self,
+                 label: str,
+                 r: np.ndarray) -> None: # currently assume fractional coordinates
         """
         Initialise an Atom instance
     

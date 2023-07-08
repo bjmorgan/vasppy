8cb699b3f685a441b3f8f0a795e985fa2dfa203e
Benjamin <b.j.morgan@bath.ac.uk>
2023-07-08 13:43:39 +0100

    KPoints class edits

--- vasppy/kpoints.py
+++ vasppy/kpoints.py
@@ -3,8 +3,7 @@ from typing import Optional
 
 
 class AutoKPoints:
-    """
-    class for automatic k-point generation data in KPOINTS.
+    """Class for automatic k-point generation data in KPOINTS.
     """
 
     def __init__(self,
@@ -12,17 +11,24 @@ class AutoKPoints:
                  subdivisions: np.ndarray,
                  grid_centering: Optional[str] = 'G',
                  shift: Optional[np.ndarray] = np.array([0., 0., 0.])) -> None:
-        """
-        Initialise an AutoKPoints object
+        """Initialise an AutoKPoints object.
 
         Args:
-            title (Str): The first line of the file, treated as a comment by VASP.
-            subdivisions: (np.Array( Int, Int, Int )): Numbers of subdivisions along each reciprocal lattice vector.
-            grid_centering (Str, optional): Specify gamma-centered (G) or the original Monkhorst-Pack scheme (MP). Default is 'G'.
-            shift: (np.Array( Float, Float, Float ), optional): Optional shift of the mesh (s_1, s_2, s_3). Default is ( [ 0., 0., 0. ] ).
+            title (str): The first line of the file, treated as a comment by VASP.
+            subdivisions: (np.ndarray(int, int, int)): 
+                Numbers of subdivisions along each reciprocal lattice vector.
+            grid_centering (str, optional):
+                Specify gamma-centered (G) or the original Monkhorst-Pack scheme (MP).
+                Default is 'G'.
+            shift: (np.ndarray(float, float, float), optional):
+                Optional shift of the mesh (s_1, s_2, s_3). Default is ( [ 0., 0., 0. ] ).
 
         Returns:
             None
+
+        Raises:
+            ValueError: If an unrecognised grid-centering option is passed in.
+
         """
         accepted_grid_centerings = ['G', 'MP']
         if grid_centering not in accepted_grid_centerings:

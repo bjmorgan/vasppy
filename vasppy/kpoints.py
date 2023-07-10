28eb447784715de2f39dc541416504cf6b16aed4
Patrick Taylor <52406436+PatrickJTaylor@users.noreply.github.com>
2023-07-10 11:00:39 +0100

    Convergence Testing (#25)
    * Add subdivisions to kspacing functionality
    
    * Add convergence_testing script
    
    * Remove redundant import
    
    * Add convergence_testing script to entry points
    
    * Improve CLI description of convergence_testing
    
    * Improve documentation and cast subdivisions to int
    
    * Add unit tests for new functions
    
    * Add missing defaults to docstrings
    
    * Add further unit tests for optional kwargs
    
    * Make allowed_kspacing immutable and type hint
    
    * Adjust unit tests for allowed_kspacing type change
    
    * Break docstring lines for readability
    
    * Add subdivisions to kspacing functionality
    
    * Add convergence_testing script
    
    * Remove redundant import
    
    * Add convergence_testing script to entry points
    
    * Improve CLI description of convergence_testing
    
    * Improve documentation and cast subdivisions to int
    
    * Add unit tests for new functions
    
    * Add missing defaults to docstrings
    
    * Add further unit tests for optional kwargs
    
    * Make allowed_kspacing immutable and type hint
    
    * Adjust unit tests for allowed_kspacing type change
    
    * Break docstring lines for readability
    
    * Remove redunant whitespace
    
    * Implement suggested styling changes
    
    * Initialise variable as float for clearer typing
    
    * Correct docstring for convergence_testing function
    
    * Relax type hint for subdivisions return value
    
    * Type: ignore for pymatgen.io.vasp imports

--- vasppy/kpoints.py
+++ vasppy/kpoints.py
@@ -1,7 +1,6 @@
 import numpy as np  # type: ignore
 from typing import Optional
 
-
 class AutoKPoints:
     """Class for automatic k-point generation data in KPOINTS."""
 
@@ -41,3 +40,52 @@ class AutoKPoints:
             self.shift = np.array([0.0, 0.0, 0.0])
         else:
             self.shift = shift
+
+def get_subdivisions_from_kspacing(kspacing: float,
+                                             reciprocal_lattice_vectors: np.ndarray) -> tuple[int, ...]:
+    """Calculate subdivisions along reciprocal lattice vectors from the miniumum allowed distance between k-points (KSPACING).
+
+    Args:
+        kspacing (float): The minimum allowed distance between k-points.
+        reciprocal_lattice_vectors (np.ndarray): The reciprocal lattice vectors. These can be retrieved from ASE as atoms.cell.reciprocal() or from Pymatgen as
+        structure.lattice.reciprocal_lattice_crystallographic.matrix.
+
+    Returns:
+        tuple[int, ...]: The subdivisions along each reciprocal lattice vector.
+    """
+    subdivisions = []
+    for row in reciprocal_lattice_vectors:
+        magnitude = np.linalg.norm(row)
+        subdivision = np.max([1, np.ceil((magnitude * 2 * np.pi) / kspacing)])
+
+        subdivisions.append(int(subdivision))
+
+    return tuple(subdivisions)
+
+def get_convergence_testing_kspacing(reciprocal_lattice_vectors: np.ndarray,
+                                     kspacing_range: tuple[float, float]=(0.1, 0.8),
+                                     step: float=0.02) -> tuple[float, ...]:
+    """Generate a range of minimum allowed distances between k-points (KSPACING) for convergence testing. This function ensures that no two values of KSPACING
+    are generated that correspond to the same k-point mesh.
+
+    Args:
+        reciprocal_lattice_vectors (np.ndarray): The reciprocal lattice vectors. These can be retrieved from ASE as atoms.cell.reciprocal() or from Pymatgen as
+        structure.lattice.reciprocal_lattice_crystallographic.matrix.
+        kspacing_range (tuple[float, float]): The minimum and maximum KSPACING values. Defaults to (0.1, 0.8).
+        step (float): The interval between KSPACING values to be tested. Defaults to 0.02.
+
+    Returns:
+        tuple[float, ...]: A range of KSPACING values which all correspond to distinct k-point grids.
+    """
+    allowed_kspacing = []
+    highest_total = 0.0
+    kspacing_min, kspacing_max = kspacing_range
+    for kspacing in np.arange(kspacing_min, kspacing_max + step, step):
+        subdivisions = get_subdivisions_from_kspacing(kspacing, reciprocal_lattice_vectors)
+        total = 1 / sum(subdivisions)
+
+        if total > highest_total:
+            allowed_kspacing.append(round(kspacing, 3))
+            highest_total = total
+
+    return tuple(allowed_kspacing)
